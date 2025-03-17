13653ce6839d951492fee777e1631537a065b225
Mikhail Ilyin <mailyin1979@gmail.com>
2025-03-17 22:41:14 +0700

    Для GroupRemovingTests созданы проверки условий

--- addressbook_web_tests/src/test/java/GroupRemovingTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovingTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -21,14 +22,33 @@ public class GroupRemovingTests {
 
     @AfterEach
     public void tearDown() {
-        //driver.findElement(By.linkText("Logout")).click();
         driver.quit();
     }
 
     @Test
     public void canRemoveGroup() {
-        driver.findElement(By.linkText("groups")).click();
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        if (! isElementPresent(By.name("selected[]"))) {
+            driver.findElement(By.name("new")).click();
+            driver.findElement(By.name("group_name")).click();
+            driver.findElement(By.name("group_name")).sendKeys("group name");
+            driver.findElement(By.name("group_header")).sendKeys("group header");
+            driver.findElement(By.name("group_footer")).sendKeys("group footer");
+            driver.findElement(By.name("submit")).click();
+            driver.findElement(By.linkText("group page")).click();
+        }
         driver.findElement(By.name("selected[]")).click();
         driver.findElement(By.xpath("(//input[@name=\'delete\'])[2]")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
     }
 }
